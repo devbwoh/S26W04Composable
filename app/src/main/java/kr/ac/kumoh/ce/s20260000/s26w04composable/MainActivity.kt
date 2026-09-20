8e0ca671366d76e45b591a571d5f19ef9fbcfb7d
devbwoh <bwoh@outlook.kr>
2026-09-20 23:47:07 +0900

    ✨카운터

--- app/src/main/java/kr/ac/kumoh/ce/s20260000/s26w04composable/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20260000/s26w04composable/MainActivity.kt
@@ -4,12 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.ce.s20260000.s26w04composable.ui.theme.S26W04ComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +44,33 @@ fun MainScreen() {
         Column(
             modifier = Modifier.padding(innerPadding)
         ) {
+            var count by remember { mutableIntStateOf(0) }
 
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = count.toString(),
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth()
+                        .background(Color(0xFFFE7A36)),
+                    color = Color.White,
+                    fontSize = 100.sp,
+                    textAlign = TextAlign.Center,
+                )
+
+                Button(
+                    modifier = Modifier.padding(8.dp).fillMaxWidth(),
+                    onClick = {
+                        count++
+                    }
+                ) {
+                    Text("증가", fontSize = 30.sp)
+                }
+            }
         }
     }
 }
